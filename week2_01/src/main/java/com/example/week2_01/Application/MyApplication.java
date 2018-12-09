f597d006df8c1322212c5f7abb0f51968695aa3f
guoxinyu <guoxinyu@qq.com>
2018-12-09 19:59:50 +0800

    bugly捕获异常

--- week2_01/src/main/java/com/example/week2_01/Application/MyApplication.java
+++ week2_01/src/main/java/com/example/week2_01/Application/MyApplication.java
@@ -7,6 +7,7 @@ import com.example.week2_01.R;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.tencent.bugly.crashreport.CrashReport;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.socialize.PlatformConfig;
 
@@ -14,8 +15,10 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //第三方登录
         UMConfigure.init(this,"5a12384aa40fa3551f0001d1","umeng",UMConfigure.DEVICE_TYPE_PHONE,"");
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        //ImageLoader全局配置
         ImageLoader.getInstance().init(new ImageLoaderConfiguration.Builder(this)
                 .memoryCacheSizePercentage(50)
                 .diskCacheSize(50*1024*1024)
@@ -28,5 +31,7 @@ public class MyApplication extends Application {
                         .showImageForEmptyUri(R.mipmap.ic_launcher)
                         .build())
                 .build());
+        //bugly捕获异常
+        CrashReport.initCrashReport(getApplicationContext(), "83950152fd", false);
     }
 }
